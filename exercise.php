a9df3aefc93ccb1380c5489c519b1697e4bb0226
Zheng Yuan <yuanzheng@sportdex.io>
2018-08-18 21:43:06 -0700

    Add some exercise

---
+++ exercise.php
@@ -0,0 +1,8 @@
+<?php
+// Store each exercise in a string variable
+// create a variable containing the day of the week
+$current_day = date('N');
+
+if ($current_day === "7") {
+    echo 'Today is Sunday';
+}
