6ca1131525c9eecc5f9edd69b617062670e2dfd8
Zheng Yuan <yuanzheng@sportdex.io>
2018-08-18 21:06:43 -0700

    Try some code with number

---
+++ numbers.php
@@ -0,0 +1,25 @@
+<?php
+$num_one = 1;
+$num_two = 2;
+$num_three = 3;
+
+//var_dump($num_one);
+//var_dump(1);
+//var_dump("1");
+//var_dump(gettype($num_one));
+//var_dump(3 + 0.2);
+//
+//$distance_home = 1.2;
+//$distance_work = 2.5;
+//var_dump($distance_home + $distance_work);
+
+// Arithmetic Operators
+$a = 5;
+$b = 10;
+var_dump($a * $b);
+var_dump($a / $b);
+
+// Auto increment
+$a = $a + 1;
+var_dump($a);
+var_dump(++$a);
