dc9dd6837bc7094a92ac6b9c6d456f058efe8ef3
Zheng Yuan <yuanzheng@sportdex.io>
2018-08-18 20:25:38 -0700

    Try some comments syntax

--- hello.php
+++ hello.php
@@ -1,2 +1,21 @@
 <?php
-echo 'Hello World!';
+/*
+ * This is file is out first "Hello World" script
+ *
+ * @author: Zheng
+ */
+
+
+// Our first php script
+//echo 'Hello World!'; //done
+echo 'Hello Treehouse'; //done
+
+//you can make a single line comment as long as you want, even wrapping to a new line, as long as you don't add a hard return
+
+// This is a second comment line
+
+/*
+ line 1
+ line 2
+ line 3
+*/
