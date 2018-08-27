69442de2d02e1daf6f8f6d6fbd13f390ece83237
Zheng Yuan <yuanzheng@sportdex.io>
2018-08-26 19:37:18 -0700

    Finish setter and getter

--- OOP/recipes.php
+++ OOP/recipes.php
@@ -3,11 +3,11 @@
 class Recipe
 {
     private $_title;
-    public $instructions = [];
-    public $ingredients = [];
-    public $source = "Zheng";
-    public $yield;
-    public $tag = [];
+    private $_instructions = [];
+    private $_ingredients = [];
+    private $_source = "Zheng";
+    private $_yield;
+    private $_tag = [];
 
     public function setTitle($title) {
         $this->_title = ucwords($title);
@@ -15,12 +15,16 @@ class Recipe
 
     public function displayRecipe()
     {
-        return $this->title . " by " . $this->source;
+        return $this->_title . " by " . $this->_source . "\n";
+    }
+
+    public function getIngredients() {
+        return $this->_ingredients;
     }
 }
 
 $recipe1 = new Recipe();
 $recipe1->setTitle('Hello world');
-var_dump($recipe1->ingredients);
+var_dump($recipe1->getIngredients());
 
 echo $recipe1->displayRecipe();
